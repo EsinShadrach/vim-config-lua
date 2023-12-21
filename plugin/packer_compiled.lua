956c9a03083a62b4341dc79ae9e5140a40556df8
EsinShadrach <esinshadrachrafe@gmail.com>
2023-12-21 19:08:51 +0100

    Added waka time

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -231,18 +231,23 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/mac/.local/share/nvim/site/pack/packer/start/vim-surround",
     url = "https://github.com/tpope/vim-surround"
+  },
+  ["vim-wakatime"] = {
+    loaded = true,
+    path = "/Users/mac/.local/share/nvim/site/pack/packer/start/vim-wakatime",
+    url = "https://github.com/wakatime/vim-wakatime"
   }
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: tokyonight
-time([[Config for tokyonight]], true)
-try_loadstring("\27LJ\2\n’\1\0\0\4\0\b\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\0016\0\0\0'\2\4\0B\0\2\0029\0\2\0005\2\6\0005\3\5\0=\3\a\2B\0\2\1K\0\1\0\foptions\1\0\0\1\0\1\ntheme\15tokyonight\flualine\1\0\1\nstyle\nnight\nsetup\15tokyonight\frequire\0", "config", "tokyonight")
-time([[Config for tokyonight]], false)
 -- Config for: lualine.nvim
 time([[Config for lualine.nvim]], true)
 try_loadstring("\27LJ\2\n—\1\0\0\a\1\n\0\19-\0\0\0\15\0\0\0X\1\2€'\0\0\0X\1\1€'\0\1\0006\1\2\0'\3\3\0B\1\2\0029\1\4\0015\3\b\0005\4\6\0'\5\5\0\18\6\0\0&\5\6\5=\5\a\4=\4\t\3B\1\2\1K\0\1\0\0\0\foptions\1\0\0\ntheme\1\0\0\16tokyonight_\nsetup\flualine\frequire\nlinux\nmacos\0", "config", "lualine.nvim")
 time([[Config for lualine.nvim]], false)
+-- Config for: tokyonight
+time([[Config for tokyonight]], true)
+try_loadstring("\27LJ\2\n’\1\0\0\4\0\b\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\0016\0\0\0'\2\4\0B\0\2\0029\0\2\0005\2\6\0005\3\5\0=\3\a\2B\0\2\1K\0\1\0\foptions\1\0\0\1\0\1\ntheme\15tokyonight\flualine\1\0\1\nstyle\nnight\nsetup\15tokyonight\frequire\0", "config", "tokyonight")
+time([[Config for tokyonight]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
