bc377852fe1693efc5c3b5849b4170ed0677fb0d
EsinShadrach <esinshadrachrafe@gmail.com>
2024-01-17 01:51:13 +0100

    added select all mapping

--- lua/rafe/remap.lua
+++ lua/rafe/remap.lua
@@ -1,3 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.api.nvim_set_keymap("n", "<C-P>", ":Format<CR>", { noremap = true, silent = true })
+
+-- a function to visually select everything
+function visualSelectAll()
+  vim.cmd [[normal! ggVG]]
+end
+
+-- Create a mapping for the function
+vim.api.nvim_set_keymap('n', '<leader>va', '<cmd>lua visualSelectAll()<CR>', { noremap = true, silent = true })
