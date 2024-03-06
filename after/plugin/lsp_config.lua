81bf0c11042b9aef6c1b8a703c07f0ddf9fb097d
EsinShadrach <esinshadrachrafe@gmail.com>
2024-03-06 12:21:39 +0100

    Telescope changes'

--- after/plugin/lsp_config.lua
+++ after/plugin/lsp_config.lua
@@ -1,13 +1,5 @@
 local lspconfig = require('lspconfig')
 
--- require("mason").setup()
--- require("mason-lspconfig").setup({
---   ensure_installed = {
---     "lua_ls",
---     "rust_analyzer"
---   },
--- })
-
 require("lspconfig").lua_ls.setup {
   -- on_attach = on_attach,
 }
@@ -21,42 +13,3 @@ lspconfig.rust_analyzer.setup {
     ['rust-analyzer'] = {},
   },
 }
-
-
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
--- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
--- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
--- vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
-
--- -- Use LspAttach autocommand to only map the following keys
--- -- after the language server attaches to the current buffer
--- vim.api.nvim_create_autocmd('LspAttach', {
---   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
---   callback = function(ev)
---     -- Enable completion triggered by <c-x><c-o>
---     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
---     -- Buffer local mappings.
---     -- See `:help vim.lsp.*` for documentation on any of the below functions
---     local opts = { buffer = ev.buf }
---     -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
---     -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
---     -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
---     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
---     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
---     vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workleader_folder, opts)
---     vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workleader_folder, opts)
---     vim.keymap.set('n', '<leader>wl', function()
---       print(vim.inspect(vim.lsp.buf.list_workleader_folders()))
---     end, opts)
---     vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
---     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
---     vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
---     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
---     vim.keymap.set('n', '<leader>f', function()
---       vim.lsp.buf.format { async = true }
---     end, opts)
---   end,
--- })
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 
 vim.api.nvim_set_keymap('n', '<leader>fw', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
