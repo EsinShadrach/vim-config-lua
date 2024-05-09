c4b49e23c0b7c6da947b385583880f5ca1f460a9
EsinShadrach <esinshadrachrafe@gmail.com>
2024-05-09 19:23:04 +0100

    the greatest commit ever

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -2,6 +2,8 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
+-- Cause of my lsp sometimes leader a fails to add
+vim.keymap.set("n", "<leader>h", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
