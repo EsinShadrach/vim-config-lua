694e82a9ebd573137080841bbcebbedc099b1a02
EsinShadrach <esinshadrachrafe@gmail.com>
2025-03-01 15:20:11 +0100

    changed command for supermaven accept

--- after/plugin/super_maven.lua
+++ after/plugin/super_maven.lua
@@ -1,7 +1,7 @@
 require("supermaven-nvim").setup({
   keymaps = {
-    accept_suggestion = "<Tab>",
-    clear_suggestion = "<C-]>",
+    accept_suggestion = "<C-]>",
+    clear_suggestion = "<C-[>",
     accept_word = "<C-j>",
   },
   ignore_filetypes = {}, -- or { "cpp", }
